c243d9c6f02243ad08e907eb2ee20b7f053429d0
gerosantacruz <geronimo.santacruz2017@gmail.com>
2020-02-03 21:36:14 -0300

    Testing completed bug fixed

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 
@@ -20,7 +21,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        #return the las five published question
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     #try:
@@ -32,6 +34,9 @@ class DetailView(generic.DetailView):
     #return render(request, 'polls/detail.html', {'question':question})
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     #question = get_object_or_404(Question, pk=question_id)
